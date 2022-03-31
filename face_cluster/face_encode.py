c2895dd224b6320f97290b12f018df6181d57bbb
Sun <js_syw@qq.com>
2022-03-31 11:48:34 +0800

    20220330-人脸聚类

--- face_cluster/face_encode.py
+++ face_cluster/face_encode.py
@@ -10,9 +10,9 @@ import os
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--dataset", required=True,
-                help="path to input directory of faces + images")
+                help="path to input directory of faces + images", default="./dataset")
 ap.add_argument("-e", "--encodings", required=True,
-                help="path to serialized db of facial encodings")
+                help="path to serialized db of facial encodings", default="encodings.pickle")
 ap.add_argument("-d", "--detection-method", type=str, default="cnn",
                 help="face detection model to use: either `hog` or `cnn`")  # 没有GPU 最好用hog
 args = vars(ap.parse_args())
@@ -28,9 +28,9 @@ for (i, imagePath) in enumerate(imagePaths):
     # load the input image and convert it from RGB (OpenCV ordering)
     # to dlib ordering (RGB)
     print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
-    print(imagePath)
-    image = cv2.imread(imagePath)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+print(imagePath)
+image = cv2.imread(imagePath)
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 # detect the (x, y)-coordinates of the bounding boxes
 # corresponding to each face in the input image
