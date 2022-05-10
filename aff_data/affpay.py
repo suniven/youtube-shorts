cadaf36832f06e357da67b6003961d3afc9cf22b
Sun <js_syw@qq.com>
2022-05-10 18:16:01 +0800

    20220510-优化

--- aff_data/affpay.py
+++ aff_data/affpay.py
@@ -18,11 +18,11 @@ from model import Affpay_Offer
 from bs4 import BeautifulSoup
 
 sqlconn = 'mysql+pymysql://root:1101syw@localhost:3306/test?charset=utf8mb4'
-url_prefix = 'https://www.affplus.com/search?verticals=Adult&sort=time_desc&page='  # 后面加页数
-# url_prefix = 'https://www.affplus.com/search?verticals=Dating&sort=time_desc&page='  # 后面加页数
-START_PAGE = 84
-END_PAGE = 100
-PAGE_COUNT = 100
+# url_prefix = 'https://www.affplus.com/search?verticals=Adult&sort=time_desc&page='  # 后面加页数
+url_prefix = 'https://www.affplus.com/search?verticals=Dating&sort=time_desc&page='  # 后面加页数
+START_PAGE = 151
+END_PAGE = 200
+PAGE_COUNT = 200
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
 }
